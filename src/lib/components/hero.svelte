bc139e559b95a830797ba32a50d5efab42377e39
Ryan Boddy <avidryanb@gmail.com>
2023-02-18 06:59:01 -0500

    Mobile sucks lol

---
+++ src/lib/components/hero.svelte
@@ -0,0 +1,10 @@
+<div></div>
+
+<style>
+    div {
+        background: url("/hero.png");
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-    import Sidebar from "../lib/components/sidebar.svelte";
+    import Hero from "../lib/components/hero.svelte";
+import Sidebar from "../lib/components/sidebar.svelte";
 
 </script>
 <div class="content">
     <Sidebar title={"Hey, I'm Ryan"} content={"A seasoned web developer who delivers fast, responsive user experiences."}/> 
+    <Hero />
 </div>
