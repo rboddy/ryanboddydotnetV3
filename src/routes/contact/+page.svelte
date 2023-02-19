186d7f7a6c2dc2046d8a0d0548d4d1bb13798a8b
Ryan Boddy <avidryanb@gmail.com>
2023-02-18 20:49:59 -0500

    Added the content page, may change it

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Sidebar from "$lib/components/sidebar.svelte";
+    import WorkViewer from "../../lib/components/work/workViewer.svelte";
+
+</script>
+<div class="content">
+    <Sidebar title={"Contact"} content={"<p>I'll get back to you as soon as I can.</p>"}/> 
+</div>
